7390fc4e7e937bffd8b37cccdcc7cb4fe90dd73e
Tony Nguyen <tcv.nguyen.2013@gmail.com>
2014-06-17 19:48:53 -0500

    Query#blocks should return root block

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,7 +1,7 @@
 class Query < ActiveRecord::Base
 
   has_one :response
-  has_many :blocks, through: :response
+  has_many :blocks, -> { where(parent_id: nil) }, through: :response
   has_many :logs
 
   scope :get_status,  ->(name) { where(status: name) }
